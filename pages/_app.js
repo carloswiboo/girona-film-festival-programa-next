54d825d222a5061c64917ddd1afbedc8df8c766a
Carlos Francisco Estrada Garcidueñas <carlos@wiboo.com.mx>
2022-11-09 08:10:39 -0600

    Subiendo google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/gironaStyle.css";
 import TagManager from "react-gtm-module";
-import React from 'react';
+import React from "react";
+import analytics from "./../utilidades/analytics";
 global.XMLHttpRequest = require("xhr2");
 
 const tagManagerArgs = {
@@ -11,7 +12,7 @@ const tagManagerArgs = {
 
 function MyApp({ Component, pageProps }) {
   React.useEffect(() => {
-    TagManager.initialize(tagManagerArgs);
+    analytics.page();
   }, []);
 
   return (
